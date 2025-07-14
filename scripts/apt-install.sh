b57b58475b43875418f7ce0194808a3a359fa06b
voidbloom13 <dakota.huyghe.dev@gmail.com>
2025-07-14 01:51:38 -0400

    refactored apt install, added zsh

--- scripts/apt-install.sh
+++ scripts/apt-install.sh
@@ -2,22 +2,21 @@
 
 # This script goes through installing all of my default programs.
 
-# Initial update/upgrade and installs nala
-sudo apt update && sudo apt upgrade -y && sudo apt-get install -y nala
-
-# Initial nala installs
-sudo nala install -y cifs-utils curl default-jdk gcc gh git kitty maven neofetch nodejs npm python3 python3-pip python3-venv tmux tree unzip
+# Initial update/upgrade and installs initial software
+sudo apt update && 
+sudo apt upgrade -y && 
+sudo apt-get install -y cifs-utils curl default-jdk gcc gh git kitty maven neofetch nodejs npm python3 python3-pip python3-venv tmux tree unzip zsh
 
 # Installs most recent version of nvim and VS Code from Snap
 if [ -f /etc/apt/preferences.d/nosnap.pref ]; then
 	sudo rm /etc/apt/preferences.d/nosnap.pref
 fi
-sudo nala update && sudo nala install -y snapd
+sudo apt update && sudo apt install -y snapd
 sudo systemctl start snapd && sudo systemctl enable snapd
 sudo snap install nvim --classic
 sudo snap install code --classic
 
-# Installs Google Chrome (I know, why use Linux and then install Chrome... I like Chrome.)
+# Installs Google Chrome
 mkdir /etc/apt/keyrings
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/keyrings/google.asc > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google.asc] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
@@ -40,60 +39,26 @@ sudo npm install -g typescript
 # Clones tmux/tpm
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
-read -p "Setup Git config? [Y]es [N]o : " gh_setup
-case "$gh_setup" in
-	[yY][eE][sS]|[yY])
-		read -p "Github Email Address: " gh_email
-		read -p "Github Username: " gh_name
-		git config --global user.email "$gh_email"
-		git config --global user.name "$gh_name"
-		echo -e "\n"
-		;;
-	[nN][oO]|[nN])
-		break
-		;;
-	*)
-		echo "Invalid input. If you wish to setup git config, please run {git config --global user.name "[git username]" && git config --global user.email [email]}"
-		;;
-esac
+# Installs favorite nerd fonts
+source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/IosevkaTerm.zip
+source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Lilex.zip
+source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Mononoki.zip
+source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/VictorMono.zip
+source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/ZedMono.zip
 
-read -p "Add Network Storage mount? [Y]es [N]o : " nas_setup
-case "$nas_setup" in
-	[yY][eE][sS]|[yY])
-		source $HOME/dotfiles/scripts/nas-setup.sh
-		;;
-	[nN][oO]|[nN])
-		break
-		;;
-	*)
-		echo "Invalid input. If you wish to run the Network Storage setup, please run {. $HOME/dotfiles/scripts/nas_setup.sh}"
-		;;
-esac
+# Creates symlinks for dotfiles
+source $HOME/dotfiles/scripts/mklinks.sh
 
-# Optionally, installs my favorite nerd fonts
-read -p "Would you like to install the following nerdfonts? { IosevkaTerm, Jetbrains Mono, Lilex, Mononoki, Victor Mono, Zed Mono } [Y]es [N]o : " install_fonts
-case "$install_fonts" in
-	[yY][eE][sS]|[yY])
-		source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/IosevkaTerm.zip
-		source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
-		source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Lilex.zip
-		source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Mononoki.zip
-		source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/VictorMono.zip
-		source $HOME/dotfiles/scripts/install-fonts.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/ZedMono.zip
-		;;
-	[nN][oO]|[nN])
-		break
-		;;
-	*)
-		echo "Invalid input. If you wish to run the Network Storage setup, please run {. $HOME/dotfiles/scripts/nas_setup.sh}"
-		;;
-esac
-
+# Sets zsh as default Shell
+zsh_loc=which zsh
+chsh -s $zsh_loc
 
 # Cleanup
-sudo nala update && sudo nala upgrade -y
+sudo apt update && sudo nala upgrade -y
 sudo apt-get clean
 sudo apt-get autoclean
 sudo apt-get autoremove
 
-. $HOME/dotfiles/.bashrc
+# Sources .bashrc, uncomment if using bash as default shell
+# . $HOME/dotfiles/.bashrc
