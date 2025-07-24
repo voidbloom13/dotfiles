e5a0ad6a163a5bff150bff3a0d8d9688521ba985
voidbloom <dakota.huyghe.dev@gmail.com>
2025-07-24 05:01:29 +0000

    added install script for ubuntu server

---
+++ scripts/ubuntu-server-install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Initial setup for home server (ubuntu server)
+
+# Initial update/upgrade and installs initial software
+sudo apt update && 
+sudo apt upgrade -y && 
+sudo apt-get install -y cifs-utils curl default-jdk gcc gh git maven neofetch nodejs npm python3 python3-pip python3-venv tmux tree unzip zsh
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Installs most recent version of nvim and from Snap
+if [ -f /etc/apt/preferences.d/nosnap.pref ]; then
+	sudo rm /etc/apt/preferences.d/nosnap.pref
+fi
+sudo apt update && sudo apt install -y snapd
+sudo systemctl start snapd && sudo systemctl enable snapd
+sudo snap install nvim --classic
+
+# Installs Node Version Manager (NVM) and NodeJS LTS
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+nvm install --lts
+nvm use --lts
+
+# Installs Typescript
+sudo npm install -g typescript
+
+# Clones tmux/tpm
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
+# Creates symlinks for dotfiles
+source $HOME/dotfiles/scripts/mklinks.sh
+
+# Sets zsh as default Shell
+zsh_loc=which zsh
+chsh -s $zsh_loc
+
+# Cleanup
+sudo apt update && sudo nala upgrade -y
+sudo apt-get clean
+sudo apt-get autoclean
+sudo apt-get autoremove
